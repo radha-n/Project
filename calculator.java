ab14cc3a0614e0f0440ef0b6629ef95e3ac7bb31
radha-n <nagaraj.radha@gmail.com>
2021-07-09 16:38:15 -0700

    trying to learn

---
+++ calculator.java
@@ -0,0 +1,99 @@
+import java.util.Scanner;
+
+ public class calculator
+  {
+    public static void main(String args[])
+     {
+       float a, b, res;
+       char choice, ch;
+       Scanner scan = new Scanner(System.in);
+	      
+      	do
+         {
+           System.out.print("1. Addition\n");
+           System.out.print("2. Subtraction\n");
+           System.out.print("3. Multiplication\n");
+           System.out.print("4. Division\n");
+           System.out.print("5. Exit\n\n");
+           System.out.print("Enter Your Choice : ");
+           choice = scan.next().charAt(0);
+           switch(choice)
+            {
+              case '1' : System.out.print("Enter Two Number : ");
+                         a = scan.nextFloat();
+                         b = scan.nextFloat();
+                         res = a + b;
+                         System.out.print("Result = " + res);
+	                 break;															                       
+	      case '2' : System.out.print("Enter Two Number : ");
+	                 a = scan.nextFloat();
+	                 b = scan.nextFloat();
+	                 res = a - b;
+	                 System.out.print("Result = " + res);
+	                 break;
+	 
+	      case '3' : System.out.print("Enter Two Number : ");
+	                 a = scan.nextFloat();
+	                 b = scan.nextFloat();
+	                 res = a * b;
+	                 System.out.print("Result = " + res);
+	                 break;
+	 
+	      case '4' : System.out.print("Enter Two Number : ");
+	                 a = scan.nextFloat();
+	                 b = scan.nextFloat();
+	                 res = a / b;
+	                 System.out.print("Result = " + res);
+	                 break;
+	 
+	      case '5' : System.exit(0);
+	                 break;
+	
+	      default : System.out.print("Wrong Choice!!!");
+	                break;
+	   }
+																	            System.out.print("\n---------------------------------------\n");
+	}while(choice != 5);       
+    }
+
+  }
+/*This is for demo purpose*/
+:X
+
+public class Calc {
+
+	int firstNumber =5, secondNumber =4;
+
+	public int sum(){
+
+		return firstNumber + secondNumber;
+
+	}
+
+	public int subtract(){
+
+		return firstNumber - secondNumber;
+
+	}
+
+	public int multiply(){
+
+		return firstNumber * secondNumber;
+
+	}
+
+	public int divide(){
+
+		return firstNumber / secondNumber;
+
+	}
+
+	public void operations()
+
+	{
+		System.out.println(sum());
+		System.out.println(subtract());
+		System.out.println(multiply());
+		System.out.println(divide());
+	}
+}
